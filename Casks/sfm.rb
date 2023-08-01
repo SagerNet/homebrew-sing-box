cdeafaf4ffc3a2f7abde1cbe6245b12300bfb377
世界 <i@sekai.icu>
2023-08-01 12:09:38 +0800

    sfm 1.3.5

--- Casks/sfm.rb
+++ Casks/sfm.rb
@@ -1,6 +1,6 @@
 cask "sfm" do
-  version "1.3.4"
-  sha256 "751456e146155f959568403ffd186524e6770e5e19f0780fc97ed664bcfc7122"
+  version "1.3.5"
+  sha256 "1a0b1c1418d59d416550402ee9451eae47f50a747b5dc526733c754f57765aa5"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-universal.zip",
       verified: "github.com/SagerNet/sing-box/"
@@ -15,6 +15,6 @@ cask "sfm" do
   uninstall quit: "io.nekohasekai.sfa.independent", login_item: "SFM"
 
   zap trash: [
-      "~/Library/Group Containers/group.io.nekohasekai.sfa",
+    "~/Library/Group Containers/group.io.nekohasekai.sfa",
   ]
 end
