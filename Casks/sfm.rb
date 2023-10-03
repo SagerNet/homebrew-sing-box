b6c29271a213dffbb884c91fe78159418742b8b1
世界 <i@sekai.icu>
2023-10-03 10:39:07 +0800

    sfm 1.5.1

--- Casks/sfm.rb
+++ Casks/sfm.rb
@@ -1,6 +1,6 @@
 cask "sfm" do
-  version "1.4.2"
-  sha256 "8bf24000adf9b1121cc037a07bbb5beb2153d9b6fa9bde9f49c21364cd4e54bb"
+  version "1.5.1"
+  sha256 "28c6721028b6cc6b8961a9891f45fefa626348aad4a2a5e7511bc3b76e7c86c3"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-universal.zip",
       verified: "github.com/SagerNet/sing-box/"
